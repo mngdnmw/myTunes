cec405a780085e2e29f9c3b528e370b279e4bdc3
Fjord82 <rfozz@hotmail.com>
2016-11-28 13:18:45 +0100

    AddContollerView
    Just made the controller for the mainView and corrected the path in the
    Main.java so it is now running.

--- src/mytunes/GUI/Controller/MainMyTunesController.java
+++ src/mytunes/GUI/Controller/MainMyTunesController.java
@@ -7,28 +7,19 @@ package mytunes.GUI.Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 /**
+ * FXML Controller class
  *
- * @author Kristoffers
+ * @author Fjord82
  */
-public class MainViewContoller implements Initializable
+public class MainMyTunesController implements Initializable
 {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event)
-    {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
--- src/mytunes/MyTunes.java
+++ src/mytunes/MyTunes.java
@@ -21,7 +21,7 @@ public class MyTunes extends Application
     @Override
     public void start(Stage stage) throws Exception
     {
-        Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("GUI/View/MainMyTunesView.fxml"));
         
         Scene scene = new Scene(root);
         
