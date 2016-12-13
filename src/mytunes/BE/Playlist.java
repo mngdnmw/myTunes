6053f52d857f3441e0c710e6e35a62347883efa9
Jeppe Ehmsen <jeppeehmsen@gmail.com>
2016-12-13 13:40:09 +0100

    Fixed ID's. Made myTunes great again.

--- src/mytunes/BE/Playlist.java
+++ src/mytunes/BE/Playlist.java
@@ -4,31 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
-import mytunes.BLL.IDManager;
+//import mytunes.BLL.IDManager;
 
 public class Playlist
 {
 
     //private int id = 0;
     //private final int id; need to come back and to change id to final 
-    private static int playlistID;
-    private String name;
+    private int playlistID;
+    private String playlistName;
     private String playlistDuration;
     private List<Integer> songsRelations;
+    private List<Song> songList;
 
 //    private final IntegerProperty id;
-    IDManager idManager = IDManager.getInstance();
+    //IDManager idManager = IDManager.getInstance();
 
-    public Playlist(String name)
+    public Playlist(int playlistID, String playlistName)
     {
-        //this.id = id;
-        this.name = name;
-        songsRelations = new ArrayList<>();
 
-        //id++;//need to change implementation
-        //id = new SimpleIntegerProperty(nextId++);
-        playlistID = idManager.getIDForPlaylist();
-        System.out.println("Playlist: " +playlistID);
+        this.playlistID = playlistID;
+        this.playlistName = playlistName;
+        
+        System.out.println(playlistName + " : " + playlistID);
 
     }
 
@@ -40,12 +38,12 @@ public class Playlist
 
     public String getName()
     {
-        return name;
+        return playlistName;
     }
 
-    public void setName(String name)
+    public void setName(String playlistName)
     {
-        this.name = name;
+        this.playlistName = playlistName;
     }
 
     public String getPlaylistDuration()
@@ -67,8 +65,4 @@ public class Playlist
     {
         this.songsRelations = songsRelations;
     }
-
-//    public int getId() {
-//        return id;
-//    }
 }
--- src/mytunes/BE/Song.java
+++ src/mytunes/BE/Song.java
@@ -2,14 +2,12 @@ package mytunes.BE;
 
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
-import mytunes.BLL.IDManager;
+//import mytunes.BLL.IDManager;
 
 import mytunes.BLL.SongManager;
 
-
 public class Song
 {
-
 
     ///also need to implement duartion
     private int songID;
@@ -21,76 +19,93 @@ public class Song
     private String songPath;
     private String readDuration;
 
-    private static int nextId = 0;
-    private final IntegerProperty songId;
-
+    //private final IntegerProperty songId;
 
 //    private static int nextId = 0;
 //    private final IntegerProperty songId;
     SongManager songManager = SongManager.getInstance();
-    IDManager idManager = IDManager.getInstance();
+    //IDManager idManager = IDManager.getInstance();
 
-    public Song(String songTitle, String songArtist, String songCategory, Long songDuration, String songPath)
+//    public Song(String songTitle, String songArtist, String songCategory, Long songDuration, String songPath)
+//    {
+//
+//        this.songTitle = songTitle;
+//        this.songArtist = songArtist;
+//        this.songCategory = songCategory;
+//        this.songDuration = songDuration; //need to work on this
+//        this.songPath = songPath;
+//
+//
+//
+//        readDuration = readableDuration(this.songDuration);
+//        songID = idManager.getIDForSong();
+//            System.out.println("Song ID: " + songID);
+//
+//    }
+    public Song(int songID, String songTitle, String songArtist, String songCategory, Long songDuration, String songPath)
     {
 
+        //this(songID, songTitle, songArtist, songCategory, songDuration, songPath);
+        this.songID = songID;
         this.songTitle = songTitle;
         this.songArtist = songArtist;
         this.songCategory = songCategory;
         this.songDuration = songDuration; //need to work on this
         this.songPath = songPath;
-
-        songId = new SimpleIntegerProperty(nextId++);
-
         readDuration = readableDuration(this.songDuration);
-        songID = idManager.getIDForSong();
-            System.out.println("Song ID: " + songID);
+
+        System.out.println("Song ID: " + songID);
 
     }
 
-    public Song(int songId, String songTitle, String songArtist, String songCategory, Long songDuration, String songPath) {
-
-        this(songTitle, songArtist, songCategory, songDuration, songPath);
-        this.songId.set(songId);
-    }
-
-    public String getReadDuration() {
+    public String getReadDuration()
+    {
         return readDuration;
     }
 
-    public int getId() {
-        return songId.get();
+    public int getId()
+    {
+        return songID;
 
     }
 
-    public String getSongTitle() {
+    public String getSongTitle()
+    {
         return songTitle;
     }
 
-    public void setSongTitle(String songTitle) {
+    public void setSongTitle(String songTitle)
+    {
         this.songTitle = songTitle;
     }
 
-    public String getSongPath() {
+    public String getSongPath()
+    {
         return songPath;
     }
 
-    public void setSongPath(String songPath) {
+    public void setSongPath(String songPath)
+    {
         this.songPath = songPath;
     }
 
-    public String getSongArtist() {
+    public String getSongArtist()
+    {
         return songArtist;
     }
 
-    public void setSongArtist(String songArtist) {
+    public void setSongArtist(String songArtist)
+    {
         this.songArtist = songArtist;
     }
 
-    public String getSongCategory() {
+    public String getSongCategory()
+    {
         return songCategory;
     }
 
-    public void setSongCategory(String songCategory) {
+    public void setSongCategory(String songCategory)
+    {
         this.songCategory = songCategory;
     }
 
@@ -115,13 +130,16 @@ public class Song
             int sec = (mili / 1000) % 60;
             int min = (mili / 1000) / 60;
 
-            if (sec < 10) {
+            if (sec < 10)
+            {
                 duration = min + ":" + "0" + sec;
-            } else {
+            } else
+            {
                 duration = min + ":" + sec;
             }
 
-        } else {
+        } else
+        {
 
             duration = "";
         }
